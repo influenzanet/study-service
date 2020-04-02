f11dc4e95187b11039f9f5d0deebde5a1cd02be5
phev8 <phev8@users.noreply.github.com>
2020-04-03 00:06:52 +0200

    implement actions for managing assigned surveys

--- studyengine/actions.go
+++ studyengine/actions.go
@@ -16,13 +16,13 @@ func ActionEval(action models.Expression, oldState models.ParticipantState, even
 	case "UPDATE_FLAG":
 		newState, err = updateFlagAction(action, oldState, event)
 	case "ADD_NEW_SURVEY":
-		err = errors.New("not implemented")
+		newState, err = addNewSurveyAction(action, oldState, event)
 	case "REMOVE_ALL_SURVEYS":
-		err = errors.New("not implemented")
+		newState, err = removeAllSurveys(action, oldState, event)
 	case "REMOVE_SURVEY_BY_KEY":
-		err = errors.New("not implemented")
+		newState, err = removeSurveyByKey(action, oldState, event)
 	case "REMOVE_SURVEYS_BY_KEY":
-		err = errors.New("not implemented")
+		newState, err = removeSurveysByKey(action, oldState, event)
 	case "ADD_REPORT":
 		err = errors.New("not implemented")
 	case "REMOVE_ALL_REPORTS":
@@ -90,8 +90,11 @@ func updateFlagAction(action models.Expression, oldState models.ParticipantState
 		return newState, err
 	}
 
-	key := k.(string)
-	value := v.(string)
+	key, ok := k.(string)
+	value, ok2 := v.(string)
+	if !ok || !ok2 {
+		return newState, errors.New("could not parse key/value")
+	}
 
 	switch key {
 	case "status":
@@ -100,5 +103,146 @@ func updateFlagAction(action models.Expression, oldState models.ParticipantState
 		return newState, fmt.Errorf("status key not known: %s", key)
 	}
 
+	return
+}
+
+// addNewSurveyAction appends a new AssignedSurvey for the participant state
+func addNewSurveyAction(action models.Expression, oldState models.ParticipantState, event models.StudyEvent) (newState models.ParticipantState, err error) {
+	newState = oldState
+	if len(action.Data) != 3 {
+		return newState, errors.New("addNewSurveyAction must have exactly three arguments")
+	}
+	evalContext := evalContext{
+		event:            event,
+		participantState: newState,
+	}
+	k, err := evalContext.expressionArgResolver(action.Data[0])
+	if err != nil {
+		return newState, err
+	}
+	start, err := evalContext.expressionArgResolver(action.Data[1])
+	if err != nil {
+		return newState, err
+	}
+	end, err := evalContext.expressionArgResolver(action.Data[2])
+	if err != nil {
+		return newState, err
+	}
+
+	surveyKey, ok1 := k.(string)
+	validFrom, ok2 := start.(float64)
+	validUntil, ok3 := end.(float64)
+
+	if !ok1 || !ok2 || !ok3 {
+		return newState, errors.New("could not parse arguments")
+	}
+
+	newSurvey := models.AssignedSurvey{
+		SurveyKey:  surveyKey,
+		ValidFrom:  int64(validFrom),
+		ValidUntil: int64(validUntil),
+	}
+	newState.AssignedSurveys = append(newState.AssignedSurveys, newSurvey)
+	return
+}
+
+// removeAllSurveys clear the assigned survey list
+func removeAllSurveys(action models.Expression, oldState models.ParticipantState, event models.StudyEvent) (newState models.ParticipantState, err error) {
+	newState = oldState
+	if len(action.Data) > 0 {
+		return newState, errors.New("addNewSurveyAction must not have arguments")
+	}
+
+	newState.AssignedSurveys = []models.AssignedSurvey{}
+	return
+}
+
+// removeSurveyByKey removes the first or last occurence of a survey
+func removeSurveyByKey(action models.Expression, oldState models.ParticipantState, event models.StudyEvent) (newState models.ParticipantState, err error) {
+	newState = oldState
+	if len(action.Data) != 2 {
+		return newState, errors.New("removeSurveyByKey must have exactly two arguments")
+	}
+	evalContext := evalContext{
+		event:            event,
+		participantState: newState,
+	}
+	k, err := evalContext.expressionArgResolver(action.Data[0])
+	if err != nil {
+		return newState, err
+	}
+	pos, err := evalContext.expressionArgResolver(action.Data[1])
+	if err != nil {
+		return newState, err
+	}
+
+	surveyKey, ok1 := k.(string)
+	position, ok2 := pos.(string)
+
+	if !ok1 || !ok2 {
+		return newState, errors.New("could not parse arguments")
+	}
+
+	as := []models.AssignedSurvey{}
+	switch position {
+	case "first":
+		found := false
+		for _, surv := range newState.AssignedSurveys {
+			if surv.SurveyKey == surveyKey {
+				if !found {
+					found = true
+					continue
+				}
+			}
+			as = append(as, surv)
+		}
+	case "last":
+		ind := -1
+		for i, surv := range newState.AssignedSurveys {
+			if surv.SurveyKey == surveyKey {
+				ind = i
+			}
+		}
+		if ind < 0 {
+			as = newState.AssignedSurveys
+		} else {
+			as = append(newState.AssignedSurveys[:ind], newState.AssignedSurveys[ind+1:]...)
+		}
+
+	default:
+		return newState, errors.New("position not known")
+	}
+	newState.AssignedSurveys = as
+	return
+}
+
+// removeSurveysByKey removes all the surveys with a specific key
+func removeSurveysByKey(action models.Expression, oldState models.ParticipantState, event models.StudyEvent) (newState models.ParticipantState, err error) {
+	newState = oldState
+	if len(action.Data) != 1 {
+		return newState, errors.New("removeSurveysByKey must have exactly one argument")
+	}
+	evalContext := evalContext{
+		event:            event,
+		participantState: newState,
+	}
+	k, err := evalContext.expressionArgResolver(action.Data[0])
+	if err != nil {
+		return newState, err
+	}
+
+	surveyKey, ok1 := k.(string)
+
+	if !ok1 {
+		return newState, errors.New("could not parse arguments")
+	}
+
+	as := []models.AssignedSurvey{}
+	for _, surv := range newState.AssignedSurveys {
+		if surv.SurveyKey != surveyKey {
+			as = append(as, surv)
+		}
+	}
+	newState.AssignedSurveys = as
 	return
 }
--- studyengine/actions_test.go
+++ studyengine/actions_test.go
@@ -2,6 +2,7 @@ package studyengine
 
 import (
 	"testing"
+	"time"
 
 	"github.com/influenzanet/study-service/models"
 )
@@ -96,16 +97,216 @@ func TestActions(t *testing.T) {
 
 	// Survey actions:
 	t.Run("ADD_NEW_SURVEY", func(t *testing.T) {
-		t.Error("test unimplemented")
+		now := time.Now().Unix()
+		action := models.Expression{
+			Name: "ADD_NEW_SURVEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey"},
+				models.ExpressionArg{DType: "num", Num: float64(now - 10)},
+				models.ExpressionArg{DType: "num", Num: float64(now + 10)},
+			},
+		}
+		newState, err := ActionEval(action, participantState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(newState.AssignedSurveys) != 1 {
+			t.Errorf("updated number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+		if newState.AssignedSurveys[0].ValidFrom != now-10 {
+			t.Errorf("unexpected validFrom: have %d, exprected: %d", newState.AssignedSurveys[0].ValidFrom, now-10)
+		}
+		if newState.AssignedSurveys[0].ValidUntil != now+10 {
+			t.Errorf("unexpected validFrom: have %d, exprected: %d", newState.AssignedSurveys[0].ValidUntil, now+10)
+		}
 	})
+
 	t.Run("REMOVE_ALL_SURVEYS", func(t *testing.T) {
-		t.Error("test unimplemented")
+		// Add surveys first
+		now := time.Now().Unix()
+		action := models.Expression{
+			Name: "ADD_NEW_SURVEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey"},
+				models.ExpressionArg{DType: "num", Num: float64(now - 10)},
+				models.ExpressionArg{DType: "num", Num: float64(now + 10)},
+			},
+		}
+		newState, err := ActionEval(action, participantState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(newState.AssignedSurveys) != 2 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+
+		// REMOVE_ALL_SURVEYS
+		action = models.Expression{
+			Name: "REMOVE_ALL_SURVEYS",
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		if len(newState.AssignedSurveys) > 0 {
+			t.Error("should not have surveys any more")
+		}
 	})
-	t.Run("REMOVE_SURVEY_BY_KEY", func(t *testing.T) {
-		t.Error("test unimplemented")
+
+	t.Run("REMOVE_SURVEY_BY_KEY last", func(t *testing.T) {
+		// Add surveys first
+		now := time.Now().Unix()
+		action := models.Expression{
+			Name: "ADD_NEW_SURVEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+				models.ExpressionArg{DType: "num", Num: float64(now - 10)},
+				models.ExpressionArg{DType: "num", Num: float64(now + 10)},
+			},
+		}
+		newState, err := ActionEval(action, participantState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		action.Data[0].Str = "testSurveyKey2"
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		action.Data[0].Str = "testSurveyKey1"
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(newState.AssignedSurveys) != 3 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+
+		// REMOVE_SURVEY_BY_KEY
+		action = models.Expression{
+			Name: "REMOVE_SURVEY_BY_KEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+				models.ExpressionArg{DType: "str", Str: "last"},
+			},
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		if len(newState.AssignedSurveys) != 2 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+		if newState.AssignedSurveys[0].SurveyKey != "testSurveyKey1" {
+			t.Errorf("unexpected survey key at pos 0: %s", newState.AssignedSurveys[0].SurveyKey)
+		}
 	})
+
+	t.Run("REMOVE_SURVEY_BY_KEY first", func(t *testing.T) {
+		// Add surveys first
+		now := time.Now().Unix()
+		action := models.Expression{
+			Name: "ADD_NEW_SURVEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+				models.ExpressionArg{DType: "num", Num: float64(now - 10)},
+				models.ExpressionArg{DType: "num", Num: float64(now + 10)},
+			},
+		}
+		newState, err := ActionEval(action, participantState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		action.Data[0].Str = "testSurveyKey2"
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		action.Data[0].Str = "testSurveyKey1"
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(newState.AssignedSurveys) != 3 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+
+		// REMOVE_SURVEY_BY_KEY
+		action = models.Expression{
+			Name: "REMOVE_SURVEY_BY_KEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+				models.ExpressionArg{DType: "str", Str: "first"},
+			},
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		if len(newState.AssignedSurveys) != 2 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+		}
+		if newState.AssignedSurveys[0].SurveyKey != "testSurveyKey2" {
+			t.Errorf("unexpected survey key at pos 0: %s", newState.AssignedSurveys[0].SurveyKey)
+		}
+	})
+
 	t.Run("REMOVE_SURVEYS_BY_KEY", func(t *testing.T) {
-		t.Error("test unimplemented")
+		// Add surveys first
+		now := time.Now().Unix()
+		action := models.Expression{
+			Name: "ADD_NEW_SURVEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+				models.ExpressionArg{DType: "num", Num: float64(now - 10)},
+				models.ExpressionArg{DType: "num", Num: float64(now + 10)},
+			},
+		}
+		newState, err := ActionEval(action, participantState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		action.Data[0].Str = "testSurveyKey2"
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(newState.AssignedSurveys) != 3 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+			return
+		}
+
+		// REMOVE_SURVEYS_BY_KEY
+		action = models.Expression{
+			Name: "REMOVE_SURVEYS_BY_KEY",
+			Data: []models.ExpressionArg{
+				models.ExpressionArg{DType: "str", Str: "testSurveyKey1"},
+			},
+		}
+		newState, err = ActionEval(action, newState, event)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+
+		if len(newState.AssignedSurveys) != 1 {
+			t.Errorf("unexpected number of surveys: %d", len(newState.AssignedSurveys))
+		}
 	})
 
 	// Report actions
