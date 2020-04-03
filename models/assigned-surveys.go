6344b06874b75fb3859521ce314c540705d212fc
phev8 <phev8@users.noreply.github.com>
2020-04-03 18:07:40 +0200

    implementing current protobuf conversions

--- models/assigned-surveys.go
+++ models/assigned-surveys.go
@@ -1,8 +1,29 @@
 package models
 
+import api "github.com/influenzanet/study-service/api"
+
 // AssignedSurvey is an object about what surveys are assigned to the participant
 type AssignedSurvey struct {
 	SurveyKey  string `bson:"surveyKey"`  // reference to the survey object
 	ValidFrom  int64  `bson:"validFrom"`  // survey should be only visible after this timestamp
 	ValidUntil int64  `bson:"validUntil"` // survey should be submitted before this timestamp
+}
+
+func (as AssignedSurvey) ToAPI() *api.AssignedSurvey {
+	return &api.AssignedSurvey{
+		SurveyKey:  as.SurveyKey,
+		ValidFrom:  as.ValidFrom,
+		ValidUntil: as.ValidUntil,
+	}
+}
+
+func AssignedSurveyFromAPI(as *api.AssignedSurvey) AssignedSurvey {
+	if as == nil {
+		return AssignedSurvey{}
+	}
+	return AssignedSurvey{
+		SurveyKey:  as.SurveyKey,
+		ValidFrom:  as.ValidFrom,
+		ValidUntil: as.ValidUntil,
+	}
 }
--- models/survey-response.go
+++ models/survey-response.go
@@ -1,6 +1,9 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/influenzanet/study-service/api"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type SurveyResponse struct {
 	ID           primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
@@ -9,4 +12,35 @@ type SurveyResponse struct {
 	SubmittedFor string               `bson:"submittedFor"`
 	SubmittedAt  int64                `bson:"submittedAt"`
 	Responses    []SurveyItemResponse `bson:"responses"`
+}
+
+func (sr SurveyResponse) ToAPI() *api.SurveyResponse {
+	resp := make([]*api.SurveyItemResponse, len(sr.Responses))
+	for i, r := range sr.Responses {
+		resp[i] = r.ToAPI()
+	}
+	return &api.SurveyResponse{
+		Key:          sr.Key,
+		SubmittedBy:  sr.SubmittedBy,
+		SubmittedFor: sr.SubmittedFor,
+		SubmittedAt:  sr.SubmittedAt,
+		Responses:    resp,
+	}
+}
+
+func SurveyResponseFromAPI(sr *api.SurveyResponse) SurveyResponse {
+	if sr == nil {
+		return SurveyResponse{}
+	}
+	resp := make([]SurveyItemResponse, len(sr.Responses))
+	for i, r := range sr.Responses {
+		resp[i] = SurveyItemResponseFromAPI(r)
+	}
+	return SurveyResponse{
+		Key:          sr.Key,
+		SubmittedBy:  sr.SubmittedBy,
+		SubmittedFor: sr.SubmittedFor,
+		SubmittedAt:  sr.SubmittedAt,
+		Responses:    resp,
+	}
 }
