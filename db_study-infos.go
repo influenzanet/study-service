54b0745b025ab988c35b1260c0d170dc5af05276
phev8 <phev8@users.noreply.github.com>
2020-04-05 15:12:27 +0200

    study db methods

---
+++ db_study-infos.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"github.com/influenzanet/study-service/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func getStudyByStudyKey(instanceID string, studyKey string) (study models.Study, err error) {
+	if err = collectionRefStudyInfos(instanceID).FindOne(
+		context.Background(),
+		bson.D{
+			primitive.E{Key: "key", Value: studyKey}, //{"studyKey", studyKey},
+		},
+		options.FindOne(),
+	).Decode(&study); err != nil {
+		return study, err
+	}
+	return
+}
+
+func getStudiesByStatus(instanceID string, status string, onlyKeys bool) (studies []models.Study, err error) {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	filter := bson.M{"status": status}
+
+	var opts *options.FindOptions
+	if onlyKeys {
+		projection := bson.D{
+			primitive.E{Key: "key", Value: 1},       // {"secretKey", 1},
+			primitive.E{Key: "secretKey", Value: 1}, // {"secretKey", 1},
+		}
+		opts = options.Find().SetProjection(projection)
+	}
+
+	cur, err := collectionRefStudyInfos(instanceID).Find(
+		ctx,
+		filter,
+		opts,
+	)
+
+	if err != nil {
+		return studies, err
+	}
+	defer cur.Close(ctx)
+
+	studies = []models.Study{}
+	for cur.Next(ctx) {
+		var result models.Study
+		err := cur.Decode(&result)
+		if err != nil {
+			return studies, err
+		}
+
+		studies = append(studies, result)
+	}
+	if err := cur.Err(); err != nil {
+		return studies, err
+	}
+
+	return studies, nil
+}
+
+func getStudySecretKey(instanceID string, studyKey string) (secretKey string, err error) {
+	projection := bson.D{
+		primitive.E{Key: "secretKey", Value: 1}, // {"secretKey", 1},
+	}
+
+	var study models.Study
+	if err = collectionRefStudyInfos(instanceID).FindOne(
+		context.Background(),
+		bson.D{
+			primitive.E{Key: "key", Value: studyKey}, //{"studyKey", studyKey},
+		},
+		options.FindOne().SetProjection(projection),
+	).Decode(&study); err != nil {
+		return "", err
+	}
+	return study.SecretKey, nil
+}
+
+func getStudyMembers(instanceID string, studyKey string) (members []models.StudyMember, err error) {
+	projection := bson.D{
+		primitive.E{Key: "members", Value: 1}, // {"members", 1},
+	}
+
+	var study models.Study
+	if err = collectionRefStudyInfos(instanceID).FindOne(
+		context.Background(),
+		bson.D{
+			primitive.E{Key: "key", Value: studyKey}, //{"studyKey", studyKey},
+		},
+		options.FindOne().SetProjection(projection),
+	).Decode(&study); err != nil {
+		return []models.StudyMember{}, err
+	}
+	return study.Members, nil
+}
+
+// saveParticipantStateDB creates or replaces the participant states in the DB
+func createStudyInDB(instanceID string, study models.Study) (models.Study, error) {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	filter := bson.M{"key": study.Key}
+	if res := collectionRefStudyInfos(instanceID).FindOne(ctx, filter); res.Err() == nil {
+		return study, errors.New("studyKey already used: %studyKey")
+	}
+
+	res, err := collectionRefStudyInfos(instanceID).InsertOne(ctx, study)
+	id, ok := res.InsertedID.(primitive.ObjectID)
+	if ok {
+		study.ID = id
+	}
+	return study, err
+}
+
+func updateStudyKeyInDB(instanceID string, oldKey string, newKey string) error {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	_, err := getStudyByStudyKey(instanceID, newKey)
+	if err == nil {
+		return errors.New("newKey already exists")
+	}
+
+	filter := bson.M{
+		"key": oldKey,
+	}
+	update := bson.M{"$set": bson.M{"key": newKey}}
+	_, err = collectionRefStudyInfos(instanceID).UpdateOne(ctx, filter, update)
+	return err
+}
+
+func updateStudyStatusInDB(instanceID string, studyKey string, status string) error {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	filter := bson.M{
+		"key": studyKey,
+	}
+	update := bson.M{"$set": bson.M{"status": status}}
+	_, err := collectionRefStudyInfos(instanceID).UpdateOne(ctx, filter, update)
+	return err
+}
+
+func updateStudyInfoInDB(instanceID string, study models.Study) (models.Study, error) {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	elem := models.Study{}
+	filter := bson.M{"key": study.Key}
+	rd := options.After
+	fro := options.FindOneAndReplaceOptions{
+		ReturnDocument: &rd,
+	}
+	err := collectionRefStudyInfos(instanceID).FindOneAndReplace(ctx, filter, study, &fro).Decode(&elem)
+	return elem, err
+}
--- models/study.go
+++ models/study.go
@@ -6,12 +6,8 @@ type Study struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Key       string             `bson:"key"`
 	SecretKey string             `bson:"secretKey"`
-
-	Members []StudyMember `bson:"members"` // users with access to manage study
-}
-
-type StudyProps struct {
-	Key string `bson:"key"`
+	Status    string             `bson:"status"`
+	Members   []StudyMember      `bson:"members"` // users with access to manage study
 }
 
 type StudyMember struct {
