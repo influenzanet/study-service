6a76f16dddd974ced8635c126189c0214d1df195
phev8 <phev8@users.noreply.github.com>
2020-04-07 16:07:55 +0200

    implement find survey responses db methods

--- db_study-responses.go
+++ db_study-responses.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/influenzanet/study-service/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func addSurveyResponseToDB(instanceID string, studyKey string, response models.SurveyResponse) error {
@@ -10,4 +15,66 @@ func addSurveyResponseToDB(instanceID string, studyKey string, response models.S
 
 	_, err := collectionRefSurveyResponses(instanceID, studyKey).InsertOne(ctx, response)
 	return err
+}
+
+type responseQuery struct {
+	ParticipantID string
+	SurveyKey     string
+	Limit         int64
+	Since         int64
+}
+
+func findSurveyResponsesInDB(instanceID string, studyKey string, query responseQuery) (responses []models.SurveyResponse, err error) {
+	ctx, cancel := getContext()
+	defer cancel()
+
+	if query.ParticipantID == "" {
+		return responses, errors.New("participant id must be defined")
+	}
+	filter := bson.M{"submittedFor": query.ParticipantID}
+
+	opts := &options.FindOptions{
+		Sort: bson.D{
+			primitive.E{Key: "submittedAt", Value: -1},
+		},
+	}
+
+	if query.Limit > 0 {
+		opts.SetLimit(query.Limit)
+	}
+
+	if len(query.SurveyKey) > 0 {
+		filter["key"] = query.SurveyKey
+	}
+
+	if query.Since > 0 {
+		filter["submittedAt"] = bson.M{"$gt": query.Since}
+	}
+
+	cur, err := collectionRefSurveyResponses(instanceID, studyKey).Find(
+		ctx,
+		filter,
+		opts,
+	)
+
+	if err != nil {
+		return responses, err
+	}
+	defer cur.Close(ctx)
+
+	responses = []models.SurveyResponse{}
+	for cur.Next(ctx) {
+		var result models.SurveyResponse
+		err := cur.Decode(&result)
+		if err != nil {
+			return responses, err
+		}
+
+		responses = append(responses, result)
+	}
+	if err := cur.Err(); err != nil {
+		return responses, err
+	}
+
+	return responses, nil
 }
--- db_study-responses_test.go
+++ db_study-responses_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"testing"
+	"time"
 
 	"github.com/influenzanet/study-service/models"
 )
@@ -32,23 +34,109 @@ func TestDbAddSurveyResponse(t *testing.T) {
 }
 
 func TestDbFindSurveyResponseForParticipant(t *testing.T) {
-	// TODO: filter for participant
-	// TODO: filter for date
-	// TODO: filter for key
+	testStudyKey := "teststudy_for_finding_responses"
+
+	surveyResps := []models.SurveyResponse{
+		// mix participants and order for submittedAt
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-30 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-32 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-29 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-23 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-6 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-5 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-6 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-7 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-15 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: "u1", SubmittedAt: time.Now().Add(-14 * time.Hour * 24).Unix()},
+	}
+	for _, sr := range surveyResps {
+		err := addSurveyResponseToDB(testInstanceID, testStudyKey, sr)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+	}
+
+	t.Run("not existing participant", func(t *testing.T) {
+		q := responseQuery{
+			ParticipantID: "u3",
+		}
+		responses, err := findSurveyResponsesInDB(testInstanceID, testStudyKey, q)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(responses) > 0 {
+			t.Errorf("unexpected number of responses found: %d", len(responses))
+		}
+	})
 
 	t.Run("find last 2 surveys with key", func(t *testing.T) {
-		t.Error("test not implemented")
+		q := responseQuery{
+			ParticipantID: "u1",
+			SurveyKey:     "s1",
+			Limit:         2,
+		}
+		responses, err := findSurveyResponsesInDB(testInstanceID, testStudyKey, q)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(responses) != 2 {
+			t.Errorf("unexpected number of responses found: %d", len(responses))
+			return
+		}
+		if responses[0].Key != responses[1].Key && responses[0].Key != q.SurveyKey {
+			t.Errorf("unexpected survey key: %s", responses[0].Key)
+		}
 	})
 
 	t.Run("find last 2 surveys without key", func(t *testing.T) {
-		t.Error("test not implemented")
+		q := responseQuery{
+			ParticipantID: "u1",
+			Limit:         2,
+		}
+		responses, err := findSurveyResponsesInDB(testInstanceID, testStudyKey, q)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(responses) != 2 {
+			t.Errorf("unexpected number of responses found: %d", len(responses))
+			return
+		}
+		if responses[0].Key == responses[1].Key {
+			t.Errorf("unexpected survey keys: %s, %s", responses[0].Key, responses[1].Key)
+		}
 	})
 
 	t.Run("find surveys after timestamp with key", func(t *testing.T) {
-		t.Error("test not implemented")
+		q := responseQuery{
+			ParticipantID: "u1",
+			SurveyKey:     "s2",
+			Since:         time.Now().Add(-20 * time.Hour * 24).Unix(),
+		}
+		responses, err := findSurveyResponsesInDB(testInstanceID, testStudyKey, q)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(responses) != 2 {
+			t.Errorf("unexpected number of responses found: %d", len(responses))
+			for _, r := range responses {
+				log.Println(r)
+			}
+			return
+		}
 	})
 
 	t.Run("find surveys after timestamp without key", func(t *testing.T) {
-		t.Error("test not implemented")
+		q := responseQuery{
+			ParticipantID: "u1",
+			Since:         time.Now().Add(-20 * time.Hour * 24).Unix(),
+		}
+		responses, err := findSurveyResponsesInDB(testInstanceID, testStudyKey, q)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(responses) != 4 {
+			t.Errorf("unexpected number of responses found: %d", len(responses))
+			return
+		}
 	})
 }
