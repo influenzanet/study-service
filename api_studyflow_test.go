aa78bb31a119cd478d964ce9a45c9129a440d754
phev8 <phev8@users.noreply.github.com>
2020-04-07 21:10:53 +0200

    implement survey context and prefill rule resolvers

--- api_studyflow_test.go
+++ api_studyflow_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"log"
 	"testing"
+	"time"
 
 	"github.com/influenzanet/study-service/api"
 	"github.com/influenzanet/study-service/models"
@@ -628,6 +630,181 @@ func TestSubmitResponseEndpoint(t *testing.T) {
 		if err != nil {
 			t.Errorf("unexpected error: %s", err.Error())
 			return
+		}
+	})
+}
+
+func TestResolveContextRules(t *testing.T) {
+	testStudyKey := "teststudy_forresolvecontext"
+	testUserID := "234234laaabbb3423"
+
+	pid1, err := userIDToParticipantID(testInstanceID, "studyfor_submitsurvey1", testUserID)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err.Error())
+		return
+	}
+	surveyResps := []models.SurveyResponse{
+		// mix participants and order for submittedAt
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-30 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-32 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-29 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-23 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-6 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-5 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-6 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: "u2", SubmittedAt: time.Now().Add(-7 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-15 * time.Hour * 24).Unix()},
+		models.SurveyResponse{Key: "s2", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-14 * time.Hour * 24).Unix()},
+	}
+	for _, sr := range surveyResps {
+		err := addSurveyResponseToDB(testInstanceID, testStudyKey, sr)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+	}
+
+	t.Run("resolve mode string arg", func(t *testing.T) {
+		testRules := models.SurveyContextDef{
+			Mode: models.ExpressionArg{Str: "test"},
+		}
+		sCtx, err := resolveContextRules(testInstanceID, testStudyKey, pid1, testRules)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if sCtx.Mode != "test" {
+			t.Errorf("unexpected mode: %s", sCtx.Mode)
+		}
+	})
+
+	t.Run("find old responses since", func(t *testing.T) {
+		testRules := models.SurveyContextDef{
+			PreviousResponses: []models.Expression{
+				models.Expression{Name: "RESPONSES_SINCE_BY_KEY", Data: []models.ExpressionArg{
+					models.ExpressionArg{DType: "num", Num: float64(time.Now().Add(-20 * time.Hour * 24).Unix())},
+					models.ExpressionArg{Str: "s2"},
+				}},
+			},
+		}
+		sCtx, err := resolveContextRules(testInstanceID, testStudyKey, pid1, testRules)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(sCtx.PreviousResponses) != 2 {
+			t.Errorf("unexpected number of surveys: %d", len(sCtx.PreviousResponses))
+		}
+	})
+
+	t.Run("find all old responses ", func(t *testing.T) {
+		testRules := models.SurveyContextDef{
+			PreviousResponses: []models.Expression{
+				models.Expression{Name: "ALL_RESPONSES_SINCE", Data: []models.ExpressionArg{
+					models.ExpressionArg{DType: "num", Num: float64(time.Now().Add(-20 * time.Hour * 24).Unix())},
+				}},
+			},
+		}
+		sCtx, err := resolveContextRules(testInstanceID, testStudyKey, pid1, testRules)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(sCtx.PreviousResponses) != 4 {
+			t.Errorf("unexpected number of surveys: %d", len(sCtx.PreviousResponses))
+		}
+	})
+
+	t.Run("find old responses by key", func(t *testing.T) {
+		testRules := models.SurveyContextDef{
+			PreviousResponses: []models.Expression{
+				models.Expression{Name: "LAST_RESPONSES_BY_KEY", Data: []models.ExpressionArg{
+					models.ExpressionArg{Str: "s1"},
+					models.ExpressionArg{DType: "num", Num: 1},
+				}},
+			},
+		}
+		sCtx, err := resolveContextRules(testInstanceID, testStudyKey, pid1, testRules)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(sCtx.PreviousResponses) != 1 {
+			t.Errorf("unexpected number of surveys: %d", len(sCtx.PreviousResponses))
+			return
+		}
+		if sCtx.PreviousResponses[0].Key != "s1" {
+			t.Errorf("unexpected survey key: %s", sCtx.PreviousResponses[0].Key)
+		}
+	})
+}
+
+func TestResolvePrefillRules(t *testing.T) {
+	testStudyKey := "teststudy_forresolveprefills"
+	testUserID := "234234laaabbb3423"
+
+	pid1, err := userIDToParticipantID(testInstanceID, "studyfor_submitsurvey1", testUserID)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err.Error())
+		return
+	}
+	surveyResps := []models.SurveyResponse{
+		// mix participants and order for submittedAt
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-6 * time.Hour * 24).Unix(), Responses: []models.SurveyItemResponse{
+			models.SurveyItemResponse{Key: "s1.1"},
+			models.SurveyItemResponse{Key: "s1.2"},
+			models.SurveyItemResponse{Key: "s1.3"},
+		}},
+		models.SurveyResponse{Key: "s2", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-5 * time.Hour * 24).Unix(), Responses: []models.SurveyItemResponse{
+			models.SurveyItemResponse{Key: "s2.1"},
+			models.SurveyItemResponse{Key: "s2.2"},
+			models.SurveyItemResponse{Key: "s2.3"},
+		}},
+		models.SurveyResponse{Key: "s1", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-15 * time.Hour * 24).Unix(), Responses: []models.SurveyItemResponse{
+			models.SurveyItemResponse{Key: "s1.1"},
+			models.SurveyItemResponse{Key: "s1.2"},
+			models.SurveyItemResponse{Key: "s1.3"},
+		}},
+		models.SurveyResponse{Key: "s2", SubmittedFor: pid1, SubmittedAt: time.Now().Add(-14 * time.Hour * 24).Unix(), Responses: []models.SurveyItemResponse{
+			models.SurveyItemResponse{Key: "s2.1"},
+			models.SurveyItemResponse{Key: "s2.2"},
+			models.SurveyItemResponse{Key: "s2.3"},
+		}},
+	}
+	for _, sr := range surveyResps {
+		err := addSurveyResponseToDB(testInstanceID, testStudyKey, sr)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+	}
+
+	t.Run("find last survey by type and extract items", func(t *testing.T) {
+		rules := []models.Expression{
+			models.Expression{
+				Name: "GET_LAST_SURVEY_ITEM", Data: []models.ExpressionArg{
+					models.ExpressionArg{Str: "s1"},
+					models.ExpressionArg{Str: "s1.1"},
+				},
+			},
+			models.Expression{
+				Name: "GET_LAST_SURVEY_ITEM", Data: []models.ExpressionArg{
+					models.ExpressionArg{Str: "s2"},
+					models.ExpressionArg{Str: "s2.2"},
+				},
+			},
+			models.Expression{
+				Name: "GET_LAST_SURVEY_ITEM", Data: []models.ExpressionArg{
+					models.ExpressionArg{Str: "s2"},
+					models.ExpressionArg{Str: "s2.4"},
+				},
+			},
+		}
+		prefill, err := resolvePrefillRules(testInstanceID, testStudyKey, pid1, rules)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+		}
+		if len(prefill.Responses) != 2 {
+			t.Errorf("unexpected number of responses: %d", len(prefill.Responses))
+			return
+		}
+		if prefill.Responses[0].Key != "s1.1" || prefill.Responses[1].Key != "s2.2" {
+			log.Println(prefill)
+			t.Error("unexpected responses")
 		}
 	})
 }
--- api_studyflow_utils.go
+++ api_studyflow_utils.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/influenzanet/study-service/models"
 	"github.com/influenzanet/study-service/studyengine"
 	"github.com/influenzanet/study-service/utils"
@@ -33,4 +36,98 @@ func getAndPerformStudyRules(instanceID string, studyKey string, pState models.P
 	}
 
 	return newState, nil
+}
+
+func resolveContextRules(instanceID string, studyKey string, participantID string, rules models.SurveyContextDef) (sCtx models.SurveyContext, err error) {
+	// mode:
+	modeRule := rules.Mode
+	switch modeRule.DType {
+	case "exp":
+		return sCtx, errors.New("expression arg type not supported yet")
+	case "str":
+		sCtx.Mode = modeRule.Str
+	default:
+		sCtx.Mode = modeRule.Str
+	}
+
+	// previous responses:
+	prevRespRules := rules.PreviousResponses
+	for _, rule := range prevRespRules {
+		switch rule.Name {
+		case "LAST_RESPONSES_BY_KEY":
+			if len(rule.Data) != 2 {
+				return sCtx, errors.New("LAST_RESPONSES_BY_KEY must have two arguments")
+			}
+			arg1 := rule.Data[0].Str
+			arg2 := int64(rule.Data[1].Num)
+			if arg1 == "" || arg2 == 0 {
+				return sCtx, errors.New("LAST_RESPONSES_BY_KEY arguments have to be defined")
+			}
+			cResps, _ := findSurveyResponsesInDB(instanceID, studyKey, responseQuery{
+				ParticipantID: participantID,
+				SurveyKey:     arg1,
+				Limit:         arg2,
+			})
+			sCtx.PreviousResponses = append(sCtx.PreviousResponses, cResps...)
+		case "ALL_RESPONSES_SINCE":
+			if len(rule.Data) != 1 {
+				return sCtx, errors.New("ALL_RESPONSES_SINCE must have one argument")
+			}
+			arg1 := int64(rule.Data[0].Num)
+			cResps, _ := findSurveyResponsesInDB(instanceID, studyKey, responseQuery{
+				ParticipantID: participantID,
+				Since:         arg1,
+			})
+			sCtx.PreviousResponses = append(sCtx.PreviousResponses, cResps...)
+		case "RESPONSES_SINCE_BY_KEY":
+			if len(rule.Data) != 2 {
+				return sCtx, errors.New("RESPONSES_SINCE_BY_KEY must have two arguments")
+			}
+			since := int64(rule.Data[0].Num)
+			surveyKey := rule.Data[1].Str
+			if surveyKey == "" || since == 0 {
+				return sCtx, errors.New("RESPONSES_SINCE_BY_KEY arguments have to be defined")
+			}
+			cResps, _ := findSurveyResponsesInDB(instanceID, studyKey, responseQuery{
+				ParticipantID: participantID,
+				SurveyKey:     surveyKey,
+				Since:         since,
+			})
+			sCtx.PreviousResponses = append(sCtx.PreviousResponses, cResps...)
+		default:
+			return sCtx, errors.New("expression is not supported yet")
+		}
+	}
+	return sCtx, nil
+}
+
+func resolvePrefillRules(instanceID string, studyKey string, participantID string, rules []models.Expression) (prefills models.SurveyResponse, err error) {
+	for _, rule := range rules {
+		switch rule.Name {
+		case "GET_LAST_SURVEY_ITEM":
+			if len(rule.Data) != 2 {
+				return prefills, errors.New("GET_LAST_SURVEY_ITEM must have two arguments")
+			}
+			surveyKey := rule.Data[0].Str
+			itemKey := rule.Data[1].Str
+			resps, err := findSurveyResponsesInDB(instanceID, studyKey, responseQuery{
+				ParticipantID: participantID,
+				SurveyKey:     surveyKey,
+				Limit:         1,
+			})
+
+			if err != nil || len(resps) < 1 {
+				continue
+			}
+			for _, item := range resps[0].Responses {
+				if item.Key == itemKey {
+					prefills.Responses = append(prefills.Responses, item)
+					continue
+				}
+			}
+		default:
+			return prefills, fmt.Errorf("expression is not supported yet: %s", rule.Name)
+		}
+	}
+	return prefills, nil
 }
