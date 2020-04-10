f87b1bc105bb3f73732dee9dc7184b4bfaae8627
phev8 <phev8@users.noreply.github.com>
2020-04-10 12:05:28 +0200

    implement user id to participant id encoding

--- utils/participant-id.go
+++ utils/participant-id.go
@@ -1,11 +1,45 @@
 package utils
 
 import (
-	"log"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"encoding/hex"
 )
+
+func createHash(key string) ([]byte, error) {
+	hasher := md5.New()
+	_, err := hasher.Write([]byte(key))
+	if err != nil {
+		return []byte{}, err
+	}
+	return hasher.Sum(nil), nil
+}
 
 // UserIDtoParticipantID encrypts userID to be used as participant ID
 func UserIDtoParticipantID(userID string, globalSecret string, studySecret string) (string, error) {
-	log.Println("UserIDtoParticipantID: not implemented")
-	return userID + "todo", nil
+	key, err := createHash(globalSecret + studySecret)
+	if err != nil {
+		return "", err
+	}
+	userIdHash, err := createHash(userID)
+	if err != nil {
+		return "", err
+	}
+
+	plaintext := []byte(userID)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := userIdHash
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	pId := hex.EncodeToString(ciphertext[aes.BlockSize:])
+	return pId, nil
 }
---
+++ utils/participant-id_test.go
@@ -0,0 +1,85 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestUserIDtoParticipantID(t *testing.T) {
+	n := 12
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		panic(err)
+	}
+	globalKey := hex.EncodeToString(b)
+	studySecret := "this!study.-a.sd"
+
+	testProfileID := primitive.NewObjectID().Hex()
+	testProfileID2 := primitive.NewObjectID().Hex()
+
+	t.Run("same user same keys", func(t *testing.T) {
+		pId, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		pId2, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		if pId != pId2 {
+			t.Errorf("pIDs don't match")
+		}
+	})
+
+	t.Run("different users same keys", func(t *testing.T) {
+		pId, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		pId2, err := UserIDtoParticipantID(testProfileID2, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		if pId == pId2 {
+			t.Errorf("pIDs shouldn't match")
+		}
+	})
+
+	t.Run("same user different study keys", func(t *testing.T) {
+		pId, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		pId2, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret+"different")
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		if pId == pId2 {
+			t.Errorf("pIDs shouldn't match")
+		}
+	})
+	t.Run("same user different global keys", func(t *testing.T) {
+		pId, err := UserIDtoParticipantID(testProfileID, globalKey, studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		pId2, err := UserIDtoParticipantID(testProfileID, globalKey+"different", studySecret)
+		if err != nil {
+			t.Errorf("unexpected error: %s", err.Error())
+			return
+		}
+		if pId == pId2 {
+			t.Errorf("pIDs shouldn't match")
+		}
+	})
+}
