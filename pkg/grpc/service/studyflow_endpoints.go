8859d8cf58eea9d6b176065d037424caf6520658
phev8 <phev8@users.noreply.github.com>
2021-12-14 20:39:22 +0100

    - updated api - remove unused previous endpoints

--- pkg/grpc/service/studyflow_endpoints.go
+++ pkg/grpc/service/studyflow_endpoints.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/coneno/logger"
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/influenzanet/go-utils/pkg/api_types"
 	"github.com/influenzanet/go-utils/pkg/constants"
 	"github.com/influenzanet/go-utils/pkg/token_checks"
@@ -79,6 +80,14 @@ func (s *studyServiceServer) EnterStudy(ctx context.Context, req *api.EnterStudy
 		resp.Surveys = append(resp.Surveys, cs)
 	}
 	return &resp, nil
+}
+
+func (s *studyServiceServer) RegisterTemporaryParticipant(ctx context.Context, _ *empty.Empty) (*api.RegisterTempParticipantResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "endpoint not implemented")
+}
+
+func (s *studyServiceServer) ConvertTemporaryToParticipant(ctx context.Context, req *api.ConvertTempParticipantReq) (*api.ServiceStatus, error) {
+	return nil, status.Error(codes.Unimplemented, "endpoint not implemented")
 }
 
 func (s *studyServiceServer) GetAssignedSurveys(ctx context.Context, req *api_types.TokenInfos) (*api.AssignedSurveys, error) {
@@ -195,145 +204,6 @@ func (s *studyServiceServer) GetAssignedSurvey(ctx context.Context, req *api.Sur
 	}
 	if len(prefill.Responses) > 0 {
 		resp.Prefill = prefill.ToAPI()
-	}
-	return &resp, nil
-}
-
-func (s *studyServiceServer) PostponeSurvey(ctx context.Context, req *api.PostponeSurveyRequest) (*api.AssignedSurveys, error) {
-	if req == nil || token_checks.IsTokenEmpty(req.Token) || req.StudyKey == "" || req.SurveyKey == "" {
-		return nil, status.Error(codes.InvalidArgument, "missing argument")
-	}
-
-	if err := utils.CheckIfProfileIDinToken(req.Token, req.ProfileId); err != nil {
-		s.SaveLogEvent(req.Token.InstanceId, req.Token.Id, loggingAPI.LogEventType_SECURITY, constants.LOG_EVENT_WRONG_PROFILE_ID, "postpone survey:"+req.ProfileId)
-		return nil, status.Error(codes.Internal, "permission denied")
-	}
-
-	// ParticipantID
-	participantID, err := s.profileIDToParticipantID(req.Token.InstanceId, req.StudyKey, req.ProfileId)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	pState, err := s.studyDBservice.FindParticipantState(req.Token.InstanceId, req.StudyKey, participantID)
-	if err != nil {
-		logger.Debug.Println("PostponeSurvey: participant state not found")
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	for i, as := range pState.AssignedSurveys {
-		if as.SurveyKey == req.SurveyKey {
-			newValidFrom := time.Now().Unix() + req.Delay
-
-			if as.ValidUntil > 0 {
-				if newValidFrom > as.ValidUntil-1800 {
-					// submit survey as empty
-					emptyResponse := types.SurveyResponse{
-						Key:           req.SurveyKey,
-						ParticipantID: participantID,
-						SubmittedAt:   time.Now().Unix(),
-						ArrivedAt:     time.Now().Unix(),
-					}
-					// perform study rules/actions
-					currentEvent := types.StudyEvent{
-						Type:       "SUBMIT",
-						Response:   emptyResponse,
-						InstanceID: req.Token.InstanceId,
-						StudyKey:   req.StudyKey,
-					}
-					pState, err = s.getAndPerformStudyRules(req.Token.InstanceId, req.StudyKey, pState, currentEvent)
-					if err != nil {
-						return nil, status.Error(codes.Internal, err.Error())
-					}
-					break
-				}
-			}
-			pState.AssignedSurveys[i].ValidFrom = newValidFrom
-		}
-	}
-
-	// save state back to DB
-	pState, err = s.studyDBservice.SaveParticipantState(req.Token.InstanceId, req.StudyKey, pState)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	resp := api.AssignedSurveys{
-		Surveys: []*api.AssignedSurvey{},
-	}
-	for _, as := range pState.AssignedSurveys {
-		cs := as.ToAPI()
-		cs.StudyKey = req.StudyKey
-		resp.Surveys = append(resp.Surveys, cs)
-	}
-
-	return &resp, nil
-}
-
-func (s *studyServiceServer) SubmitStatusReport(ctx context.Context, req *api.StatusReportRequest) (*api.AssignedSurveys, error) {
-	if req == nil || token_checks.IsTokenEmpty(req.Token) || req.StatusSurvey == nil {
-		return nil, status.Error(codes.InvalidArgument, "missing argument")
-	}
-
-	if err := utils.CheckIfProfileIDinToken(req.Token, req.ProfileId); err != nil {
-		s.SaveLogEvent(req.Token.InstanceId, req.Token.Id, loggingAPI.LogEventType_SECURITY, constants.LOG_EVENT_WRONG_PROFILE_ID, "submit status report:"+req.ProfileId)
-		return nil, status.Error(codes.Internal, "permission denied")
-	}
-
-	studies, err := s.studyDBservice.GetStudiesByStatus(req.Token.InstanceId, "active", true)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-	resp := api.AssignedSurveys{
-		Surveys: []*api.AssignedSurvey{},
-	}
-	for _, study := range studies {
-		participantID, err := utils.ProfileIDtoParticipantID(req.ProfileId, s.StudyGlobalSecret, study.SecretKey, study.Configs.IdMappingMethod)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-
-		pState, err := s.studyDBservice.FindParticipantState(req.Token.InstanceId, study.Key, participantID)
-		if err != nil {
-			// user not in the study - log.Println(err)
-			continue
-		}
-
-		if pState.StudyStatus != types.PARTICIPANT_STUDY_STATUS_ACTIVE {
-			continue
-		}
-
-		// Save responses
-		response := types.SurveyResponseFromAPI(req.StatusSurvey)
-		response.ParticipantID = participantID
-		err = s.studyDBservice.AddSurveyResponse(req.Token.InstanceId, study.Key, response)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-
-		// perform study rules/actions
-		currentEvent := types.StudyEvent{
-			Type:       "SUBMIT",
-			Response:   response,
-			InstanceID: req.Token.InstanceId,
-			StudyKey:   study.Key,
-		}
-		pState, err = s.getAndPerformStudyRules(req.Token.InstanceId, study.Key, pState, currentEvent)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-
-		// save state back to DB
-		pState, err = s.studyDBservice.SaveParticipantState(req.Token.InstanceId, study.Key, pState)
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
-		}
-
-		for _, as := range pState.AssignedSurveys {
-			cs := as.ToAPI()
-			cs.StudyKey = study.Key
-			resp.Surveys = append(resp.Surveys, cs)
-		}
 	}
 	return &resp, nil
 }
