506b9ea07e47876dfe365f3b091af1dd75947215
phev8 <phev8@users.noreply.github.com>
2021-12-23 14:28:50 +0100

    include messages attribute to participant state

--- pkg/types/participant-state.go
+++ pkg/types/participant-state.go
@@ -11,11 +11,18 @@ const (
 // ParticipantState defines the datamodel for current state of the participant in a study as stored in the database
 type ParticipantState struct {
 	ID              primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
-	ParticipantID   string               `bson:"participantID"` // reference to the study specific participant ID
-	EnteredAt       int64                `bson:"enteredAt"`
-	StudyStatus     string               `bson:"studyStatus"` // shows if participant is active in the study - possible values: "active", "temporary", "exited". Other values are possible and are handled like "exited" on the server.
-	Flags           map[string]string    `bson:"flags"`
-	AssignedSurveys []AssignedSurvey     `bson:"assignedSurveys"`
-	Reports         []SurveyItemResponse `bson:"reports"`
-	LastSubmissions map[string]int64     `bson:"lastSubmission"` // surveyKey with timestamp
+	ParticipantID   string               `bson:"participantID" json:"participantID"` // reference to the study specific participant ID
+	EnteredAt       int64                `bson:"enteredAt" json:"enteredAt"`
+	StudyStatus     string               `bson:"studyStatus" json:"studyStatus"` // shows if participant is active in the study - possible values: "active", "temporary", "exited". Other values are possible and are handled like "exited" on the server.
+	Flags           map[string]string    `bson:"flags" json:"flags"`
+	AssignedSurveys []AssignedSurvey     `bson:"assignedSurveys" json:"assignedSurveys"`
+	Reports         []SurveyItemResponse `bson:"reports" json:"reports"`
+	LastSubmissions map[string]int64     `bson:"lastSubmission" json:"lastSubmission"` // surveyKey with timestamp
+	Messages        []ParticipantMessage `bson:"messages" json:"messages"`
+}
+
+type ParticipantMessage struct {
+	ID           string `bson:"id" json:"id"`
+	Type         string `bson:"type" json:"type"`
+	ScheduledFor int64  `bson:"scheduledFor" json:"scheduledFor"`
 }
