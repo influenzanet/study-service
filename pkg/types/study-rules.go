971f7c322ecfbc1c67a72f86d9e3e0e1341a8d72
phev8 <phev8@users.noreply.github.com>
2024-03-06 22:03:25 +0100

    update study rule data model to include serialised version of the rules

--- pkg/types/study-rules.go
+++ pkg/types/study-rules.go
@@ -1,19 +1,26 @@
 package types
 
 import (
+	"encoding/json"
+
 	"github.com/influenzanet/study-service/pkg/api"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type StudyRules struct {
-	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	StudyKey   string             `bson:"studyKey"`
-	UploadedAt int64              `bson:"uploadedAt"`
-	UploadedBy string             `bson:"uploadingUser"`
-	Rules      []Expression       `bson:"rules"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	StudyKey        string             `bson:"studyKey" json:"studyKey"`
+	UploadedAt      int64              `bson:"uploadedAt" json:"uploadedAt"`
+	UploadedBy      string             `bson:"uploadedBy" json:"uploadedBy"`
+	Rules           []Expression       `bson:"rules,omitempty" json:"rules"`
+	SerialisedRules string             `bson:"serialisedRules,omitempty" json:"serialisedRules,omitempty"`
 }
 
 func (sr StudyRules) ToAPI() *api.StudyRules {
+	if sr.Rules == nil {
+		sr.UnmarshalRules()
+	}
+
 	rules := make([]*api.Expression, len(sr.Rules))
 	for i, r := range sr.Rules {
 		rules[i] = r.ToAPI()
@@ -26,4 +33,29 @@ func (sr StudyRules) ToAPI() *api.StudyRules {
 		UploadedBy: sr.UploadedBy,
 		Rules:      rules,
 	}
+}
+
+func (studyRules *StudyRules) MarshalRules() error {
+	rulesString, err := json.Marshal(studyRules.Rules)
+	if err != nil {
+		return err
+	}
+	studyRules.SerialisedRules = string(rulesString)
+	studyRules.Rules = nil
+	return nil
+}
+
+func (studyRules *StudyRules) UnmarshalRules() error {
+	if studyRules.SerialisedRules == "" && studyRules.Rules == nil {
+		studyRules.Rules = []Expression{}
+		return nil
+	}
+	var rules []Expression
+	err := json.Unmarshal([]byte(studyRules.SerialisedRules), &rules)
+	if err != nil {
+		return err
+	}
+	studyRules.Rules = rules
+	studyRules.SerialisedRules = ""
+	return nil
 }
